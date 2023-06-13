7ba3805399ae963544cb5fcc7ba8f1be5b3d1d4b
Humberto Rivero <humbertorivero9@gmail.com>
2023-06-13 19:44:54 -0300

    Add styles form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,17 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import {Inter} from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({subsets: ["latin"]});
 
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <h1>React Hook Form</h1>
-    </main>
-  )
+	return (
+		<main
+			className={`flex min-h-screen flex-col items-center p-24 ${inter.className}`}
+		>
+			<h1>React Hook Form</h1>
+			<form>
+				
+			</form>
+		</main>
+	);
 }
