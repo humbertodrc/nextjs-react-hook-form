c365a206cde8c6a832aa26580b3d123fb208798d
Humberto Rivero <humbertorivero9@gmail.com>
2023-06-15 20:22:09 -0300

    Add yup

---
+++ src/components/Form copy.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+interface Inputs{
+  firstName: string;
+  lastName: string;
+  email: string;
+  gender: string;
+}
+
+export const Form = () => {
+
+  const { register, handleSubmit, formState:{errors}} = useForm<Inputs>();
+
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+  const onsubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onsubmit)}>
+      <div>
+        <label htmlFor="firstName">Nombre:</label>
+        <input type="text" id="firstName" {...register('firstName', { required: true, minLength: 3, maxLength: 10 })} />
+        {errors.firstName?.type === 'required' && <span>Este campo es requerido</span>}
+        {errors.firstName?.type === 'minLength' && <span>El nombre debe tener al menos 3 caracteres</span>}
+        {errors.firstName?.type === 'maxLength' && <span>El nombre debe tener menos de 10 caracteres</span>}
+      </div>
+      <div>
+        <label htmlFor="lastName">Apellido:</label>
+        <input type="text" id="lastName" {...register('lastName', {required: true, minLength: 5})} />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" {...register('email', {required: true, pattern: regex})} />
+      </div>
+      <div>
+      <select {...register("gender")}>
+        <option value="female">female</option>
+        <option value="male">male</option>
+        <option value="other">other</option>
+      </select>
+      </div>
+      <button type="submit">Enviar</button>
+		</form>
+  )
+}
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,48 +1,151 @@
-import React from 'react'
-import { useForm, SubmitHandler } from 'react-hook-form';
+import {
+	Box,
+	FormControl,
+	InputLabel,
+	MenuItem,
+	Select,
+	TextField,
+	Typography,
+} from "@mui/material";
+import React from "react";
+import { useForm, SubmitHandler, Controller } from "react-hook-form";
 
-interface Inputs{
-  firstName: string;
-  lastName: string;
-  email: string;
-  gender: string;
+// Yup
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+
+interface Inputs {
+	firstName: string;
+	lastName: string;
+	email: string;
+	gender: string;
 }
 
 export const Form = () => {
 
-  const { register, handleSubmit, formState:{errors}} = useForm<Inputs>();
+  const schema = yup.object({
+    firstName: yup.string().required().min(3, "Debe ser mayor a 3 caracteres").max(10),
+    lastName: yup.string().required(),
+    email: yup.string().email("Ingrese un correo valido").required().matches(/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "Ingrese un correo valido"),
+    gender: yup.string().required()
+  })
 
-  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+  type FormData = yup.InferType<typeof schema>;
 
-  const onsubmit: SubmitHandler<Inputs> = (data) => {
-    console.log(data);
-  }
+	const {
+		register,
+		handleSubmit,
+		formState: {errors},
+		control,
+	} = useForm<FormData>({resolver: yupResolver(schema)});
 
-  return (
-    <form onSubmit={handleSubmit(onsubmit)}>
-      <div>
-        <label htmlFor="firstName">Nombre:</label>
-        <input type="text" id="firstName" {...register('firstName', { required: true, minLength: 3, maxLength: 10 })} />
-        {errors.firstName?.type === 'required' && <span>Este campo es requerido</span>}
-        {errors.firstName?.type === 'minLength' && <span>El nombre debe tener al menos 3 caracteres</span>}
-        {errors.firstName?.type === 'maxLength' && <span>El nombre debe tener menos de 10 caracteres</span>}
-      </div>
-      <div>
-        <label htmlFor="lastName">Apellido:</label>
-        <input type="text" id="lastName" {...register('lastName', {required: true, minLength: 5})} />
-      </div>
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" {...register('email', {required: true, pattern: regex})} />
-      </div>
-      <div>
-      <select {...register("gender")}>
-        <option value="female">female</option>
-        <option value="male">male</option>
-        <option value="other">other</option>
-      </select>
-      </div>
-      <button type="submit">Enviar</button>
-		</form>
-  )
-}
+	const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+	console.log(errors);
+
+	const onsubmit: SubmitHandler<FormData> = (data: FormData) => {
+		console.log(data);
+	};
+
+	return (
+		<Box sx={{width: "800px"}}>
+			<form onSubmit={handleSubmit(onsubmit)} style={{width: "100%"}}>
+				<Controller
+					name="firstName"
+					control={control}
+					defaultValue=""
+					rules={{required: true, minLength: 3, maxLength: 10}}
+					render={({field}) => (
+						<TextField
+							{...field}
+							id="firstName"
+							label="Nombre"
+              variant="outlined"
+              fullWidth
+						/>
+					)}
+				/>
+				<Typography variant="body2" color="error">
+					{errors.firstName?.type === "required" && (
+						<span>Este campo es requerido</span>
+					)}
+					{errors.firstName?.type === "minLength" && (
+						<span>El nombre debe tener al menos 3 caracteres</span>
+					)}
+					{errors.firstName?.type === "maxLength" && (
+						<span>El nombre debe tener menos de 10 caracteres</span>
+					)}
+          {errors.firstName?.message}
+				</Typography>
+				<Controller
+					name="lastName"
+					control={control}
+					defaultValue=""
+					rules={{required: true, minLength: 5}}
+					render={({field}) => (
+						<TextField
+							{...field}
+							id="firstName"
+							label="Nombre"
+              variant="outlined"
+              fullWidth
+						/>
+					)}
+				/>
+				<Typography variant="body2" color="error">
+					{errors.lastName?.type === "required" && (
+						<span>Este campo es requerido</span>
+					)}
+					{errors.lastName?.type === "minLength" && (
+						<span>El apellido debe tener al menos 5 caracteres</span>
+					)}
+				</Typography>
+				<Controller
+					name="email"
+					control={control}
+					defaultValue=""
+					rules={{required: true, pattern: regex}}
+					render={({field}) => (
+            <TextField
+              {...field}
+              id="email"
+              type='email'
+              label="Email"
+              variant="outlined"
+              fullWidth
+            />
+					)}
+				/>
+				<Typography variant="body2" color="error">
+					{errors.email?.type === "required" && (
+						<span>Este campo es requerido</span>
+					)}
+					{errors.email?.type === "pattern" && (
+						<span>El email debe tener un formato válido</span>
+					)}
+				</Typography>
+				<FormControl sx={{m: 1, minWidth: 80}}>
+					<InputLabel id="demo-simple-select-autowidth-label">Age</InputLabel>
+					<Controller
+						name="gender"
+						control={control}
+						defaultValue=""
+						render={({field}) => (
+							<Select
+								{...field}
+								autoWidth
+								defaultValue=""
+                labelId="demo-simple-select-autowidth-label"
+							>
+								<MenuItem value="female">Femenino</MenuItem>
+								<MenuItem value="male">Masculino</MenuItem>
+								<MenuItem value="other">Otro</MenuItem>
+							</Select>
+						)}
+					/>
+				</FormControl>
+				<button type="submit">Enviar</button>
+			</form>
+		</Box>
+	);
+};
